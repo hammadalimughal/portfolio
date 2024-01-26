b1590170bd40386026a4940206ff16dab4d6b0e8
Hammad Ali <hammad19ali@gmail.com>
2024-01-26 21:59:13 +0500

    added draw svg usin framer-motion

--- src/components/Doodle1.js
+++ src/components/Doodle1.js
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 const Doodle1 = ({ style, className }) => {
-    return (
-        <svg style={style} className={className} width="688" height="61" viewBox="0 0 688 61" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 51C113.837 26.5985 392.808 -12.4606 678 26.5152" stroke="#DFFB6D" stroke-width="19" stroke-linecap="round" />
-        </svg>
-    )
-}
+    const draw = {
+        hidden: { pathLength: 0, opacity: 0 },
+        visible: (i) => {
+            // const delay = 1 + i * 0.5;
+            const delay = 1;
+            return {
+                pathLength: 1,
+                opacity: 1,
+                transition: {
+                    pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+                    opacity: { delay, duration: 0.01 }
+                }
+            };
+        }
+    };
 
-export default Doodle1
+    return (
+        <motion.svg
+            initial="hidden"
+            animate="visible"
+            style={style}
+            className={className}
+            width="688"
+            height="61"
+            viewBox="0 0 688 61"
+            fill="none"
+        >
+            <motion.path
+                variants={draw}
+                d="M10 51C113.837 26.5985 392.808 -12.4606 678 26.5152" stroke="#DFFB6D" strokeWidth="19" strokeLinecap="round" />
+        </motion.svg>
+    );
+};
+
+export default Doodle1;
--- src/components/Homebanner.js
+++ src/components/Homebanner.js
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import bannerImg from '../images/me.png'
 import dots from '../images/dots.png'
 import { Row, Col } from 'antd'
 import { Link } from 'react-router-dom'
 import { RightOutlined } from '@ant-design/icons'
 import Doodle1 from './Doodle1'
+import gsap from 'gsap'
 
 const Homebanner = () => {
+    const image = useRef(null)
+    useEffect(() => {
+        gsap.fromTo(image.current, {
+            y: "100%"
+        }, {
+            y: "0",
+            delay: 1,
+            duration: 1
+        })
+    }, [])
     return (
         <>
             <section className="home-banner">
                 <div className="container">
                     <div className="banner-content">
-                        <Row style={{alignItems: 'center', justifyContent: 'space-between'}}>
+                        <Row style={{ alignItems: 'center', justifyContent: 'space-between' }}>
                             <Col span={14}>
                                 <h5 className="theme-h5">Hammad Ali Mughal — </h5>
                                 {/* <h1 className="theme-h1">Build your great portfolio <span>website.</span></h1> */}
                                 <h1 className="theme-h1">
                                     <Doodle1 />
-                                    MERN Stack Developer  <br /> Web <span>Enthusiast</span></h1>
+                                    MERN Stack 
+                                    Developer  <br /> Web <span>Enthusiast</span></h1>
                                 <p className="theme-p">Hello, I'm <strong>Hammad Ali</strong>, a passionate and results-driven <strong>Web & Mobile App</strong> Developer having <strong>3+</strong> years of experience. With a strong foundation in web development, I specialize inbuilding  <strong>modern and intuitive</strong> digital experiences.</p>
                                 <Link to="/contact" className="contact-aquire">Hire Me <RightOutlined /></Link>
                             </Col>
                             <Col span={8}>
                                 <div className="img-wrapper">
                                     <img src={dots} alt="" className="dots" />
-                                    <img className="img-fluid" src={bannerImg} alt="" />
+                                    <div className="my-img">
+                                    <img ref={image} className="img-fluid" src={bannerImg} alt="" />
+                                    </div>
                                 </div>
                             </Col>
                         </Row>
